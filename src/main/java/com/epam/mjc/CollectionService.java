bf62c14812cef1a2a9ac17a63ed6eade109e521d
sergey3321 <mail.sergey21@gmail.com>
2024-01-25 17:57:51 +0300

    stage1-module5-task3

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,67 @@
 package com.epam.mjc;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        if (list.isEmpty()) {
+            return Collections.emptyList();
+        }
+        try (Stream<Integer> stream = list.stream(); Stream<Integer> evenStream = stream.filter(n -> n % 2 == 0)) {
+            List<Integer> evenList = evenStream.collect(Collectors.toList());
+            return evenList;
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+            return Collections.emptyList();
+        }
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        if (list.isEmpty()) {
+            return Collections.emptyList();
+        }
+        try (Stream<String> stream = list.stream(); Stream<String> upperStream = stream.map(s -> s.toUpperCase())) {
+            List<String> upperList = upperStream.collect(Collectors.toList());
+            return upperList;
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+            return Collections.emptyList();
+        }
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        try (Stream<Integer> stream = list.stream()) {
+            Optional<Integer> max = stream.max(Integer::compare);
+            return max;
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+            return Optional.empty();
+        }
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        try (Stream<List<Integer>> stream = list.stream(); Stream<Integer> flatStream = stream.flatMap(l -> l.stream())) {
+            Optional<Integer> min = flatStream.min(Integer::compare);
+            return min;
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+            return Optional.empty();
+        }
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        try (Stream<Integer> stream = list.stream()) {
+            Integer result = stream.reduce(0, Integer::sum, Integer::sum);
+            return result;
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+            return null;
+        }
     }
 }
+
